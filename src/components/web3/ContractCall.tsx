244046161327fc3eb5a77ac8308c55fec696d835
Kodziak <kodziak1416@gmail.com>
2023-05-27 16:26:51 +0200

    cleaning

--- src/components/web3/ContractCall.tsx
+++ src/components/web3/ContractCall.tsx
@@ -1,34 +1,28 @@
-import { contractTx, useInkathon, useContract, contractCallDryRun, contractQuery} from '@scio-labs/use-inkathon'
-
-import TokenABI from '../../abis/Token.json';
-import ShielderABI from '../../abis/Shielder.json';
+import { useInkathon, useContract, contractQuery} from '@scio-labs/use-inkathon'
+import {deposit, Deposit} from 'shielder-sdk';
 import { Button, Flex, Text } from '@chakra-ui/react';
 
-import { BN, BN_ONE } from "@polkadot/util";
 import { WeightV2 } from '@polkadot/types/interfaces';
-
 import { ContractPromise } from "@polkadot/api-contract";
 import { useState } from 'react';
 
-const MAX_CALL_WEIGHT = new BN(5_000_000_000_000).isub(BN_ONE);
-const PROOFSIZE = new BN(1_000_000);
-
-const shielderContractAddress = '5ETqdUdd2TLJuHxvXERxqHWh9M5ExKT5xpyJY69v1xSfso9d';
-const tokenContractAddress = '5HZPNyNBLb7hZLGpERKUPExWCiBQiRAfhaY3X1UimDZpVJxs';
-
-import {deposit, Deposit} from 'shielder-sdk';
+import TokenABI from '../../abis/Token.json';
+import ShielderABI from '../../abis/Shielder.json';
+import { TOKEN_CONTRACT_ADDRESS, SHIELDER_CONTRACT_ADDRESS, MAX_CALL_WEIGHT, PROOFSIZE } from '@constants';
 
 export default function ContractCall() {
   const [allowance, setAllowance] = useState('0');
+  const [leaf, setLeaf] = useState(0);
   const {api, activeAccount} = useInkathon();
-  const contract = useContract(TokenABI, tokenContractAddress);
+  const tokenContract = useContract(TokenABI, TOKEN_CONTRACT_ADDRESS);
+  const shielderContract = useContract(ShielderABI, SHIELDER_CONTRACT_ADDRESS);
 
   const getCurrentAllowance = async () => {
     if (api) {
       const contractApi = new ContractPromise(
         api,
         TokenABI,
-        tokenContractAddress
+        TOKEN_CONTRACT_ADDRESS
       );
 
       const { gasRequired } = await contractApi.query['psp22::increaseAllowance'](
@@ -40,7 +34,7 @@ export default function ContractCall() {
           }) as WeightV2,
           storageDepositLimit: null,
         },
-        shielderContractAddress, 10
+        SHIELDER_CONTRACT_ADDRESS, 10
       );
   
       const gasLimit = api?.registry.createType(
@@ -48,10 +42,10 @@ export default function ContractCall() {
         gasRequired
       ) as WeightV2;
   
-      const res = await contractQuery(api, activeAccount?.address!, contract?.contract!, 'psp22::allowance', {
+      const res = await contractQuery(api, activeAccount?.address!, tokenContract?.contract!, 'psp22::allowance', {
         gasLimit,
         storageDepositLimit: null,
-      }, [activeAccount?.address!, shielderContractAddress])
+      }, [activeAccount?.address!, SHIELDER_CONTRACT_ADDRESS])
   
       console.log(res.output?.toJSON())
       setAllowance(res.output?.toJSON()?.ok);
@@ -60,22 +54,14 @@ export default function ContractCall() {
   }
 
   const increaseAllowance = async () => {
-    console.log('call contract');
     await getCurrentAllowance();
 
     if(api) {
-
       const contractApi = new ContractPromise(
         api,
         TokenABI,
-        tokenContractAddress
+        TOKEN_CONTRACT_ADDRESS
       );
-
-      // const gasLimit = api?.registry.createType("WeightV2", {
-      //   refTime: MAX_CALL_WEIGHT,
-      //   proofSize: PROOFSIZE,
-      // }) as WeightV2;
-      // const storageDepositLimit = null;
 
       const { gasRequired, result, output } = await contractApi.query['psp22::increaseAllowance'](
         activeAccount?.address!,
@@ -86,24 +72,20 @@ export default function ContractCall() {
           }) as WeightV2,
           storageDepositLimit: null,
         },
-        shielderContractAddress, 10
+        SHIELDER_CONTRACT_ADDRESS, 10
       );
-
-      // console.log({gasRequired, result, output});
 
       const gasLimit = api?.registry.createType(
         "WeightV2",
         gasRequired
       ) as WeightV2;
 
-      // console.log(api, activeAccount, contract);
-
       const queryTx = await contractApi.tx['psp22::increaseAllowance'](
         {
           gasLimit,
           storageDepositLimit: null,
         },
-        shielderContractAddress, 10
+        SHIELDER_CONTRACT_ADDRESS, 10
       );
     
       await queryTx.signAndSend(activeAccount?.address!, async (res) => {
@@ -115,34 +97,12 @@ export default function ContractCall() {
           console.log(res.toHuman());
         }
       });
-
-      
-
-      // const contractCallDryRunResult = await contractCallDryRun(api!, activeAccount?.address!, contract.contract!, 'psp22::increaseAllowance', {
-      //   gasLimit,
-      //   storageDepositLimit
-      // }, ['5DeiFTx5mFBYqWvNECbZqnV4fZsvCQ7qgu3fp6F8HEY7ccHM', 10]);
-    
-      
-      // console.log(contractCallDryRunResult.result?.toHuman())
-  
-      
-  
-      // const contractTxResult = await contractTx(api!, activeAccount?.address!, contract.contract!, 'PSP22::increase_allowance', {
-      //   gasLimit,
-      //   storageDepositLimit
-      // }, ['5DeiFTx5mFBYqWvNECbZqnV4fZsvCQ7qgu3fp6F8HEY7ccHM', 10]);
-  
-      // console.log(contractTxResult.result?.toHuman());
     } else {
       console.log('api is not defined');
     }
   }
 
   const depositTokens = async () => {
-    console.log('call deposit');
-    // TODO: Generate PROOF
-
     if(api) {
       const dep: Deposit = {
         token_id: 0,
@@ -151,15 +111,12 @@ export default function ContractCall() {
       };
 
       const depositWasmResult = await deposit(dep);
-      console.log(depositWasmResult);
       const depositWASMJSON = JSON.parse(depositWasmResult);
-
-      console.log(depositWASMJSON);
 
       const contractApi = new ContractPromise(
         api,
         ShielderABI,
-        shielderContractAddress
+        SHIELDER_CONTRACT_ADDRESS
       );
 
       const { gasRequired, result, output } = await contractApi.query['deposit'](
@@ -174,15 +131,19 @@ export default function ContractCall() {
         // tokenid, value, note, proof
         0, 10, depositWASMJSON.note.map((not: number) => `${not}`), `0x${depositWASMJSON.proof}`
       );
-
       
       const gasLimit = api?.registry.createType(
         "WeightV2",
         gasRequired
       ) as WeightV2;
 
-
-      console.log({gasRequired, result, output});
+      const res = await contractQuery(api, activeAccount?.address!, shielderContract?.contract!, 'deposit', {
+        gasLimit,
+        storageDepositLimit: null,
+      }, [0, 10, depositWASMJSON.note.map((not: number) => `${not}`), `0x${depositWASMJSON.proof}`])
+  
+      console.log('leaf', res.output?.toJSON());
+      setLeaf(res.output?.toJSON().ok);
 
       const queryTx = await contractApi.tx['deposit'](
           {
@@ -196,20 +157,13 @@ export default function ContractCall() {
           if (res.status.isInBlock) {
             console.log("in a block");
           } else if (res.status.isFinalized) {
-            console.log("finalized");
-            // console.log(`STATUS_TO_JSON: ${res.status.toJSON()}`);
-            console.log(res.toHuman());
-            console.log(res);
-            // console.log(`EEEVVVEEENNTTTYYY: ${res.events}`);
+            console.log("deposit finalized");
           }
         });
 
     } else {
       console.log('api is not defined');
     }
-
-    
-
   }
 
   return (
---
+++ src/constants/index.ts
@@ -0,0 +1,7 @@
+import { BN, BN_ONE } from "@polkadot/util";
+
+export const MAX_CALL_WEIGHT = new BN(5_000_000_000_000).isub(BN_ONE);
+export const PROOFSIZE = new BN(1_000_000);
+
+export const SHIELDER_CONTRACT_ADDRESS = '5ETqdUdd2TLJuHxvXERxqHWh9M5ExKT5xpyJY69v1xSfso9d';
+export const TOKEN_CONTRACT_ADDRESS = '5HZPNyNBLb7hZLGpERKUPExWCiBQiRAfhaY3X1UimDZpVJxs';
